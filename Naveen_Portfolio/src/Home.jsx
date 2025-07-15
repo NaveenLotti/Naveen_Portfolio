58fbd84fe0798c2b09586ba1475ac560d0a52684
NaveenLotti <naveenlotti02@gmail.com>
2025-07-15 21:10:13 +0530

    Initialize React + Vite portfolio project
    Set up a new React project using Vite, including initial configuration files, ESLint setup, and basic components for Home, Header, Footer, and dynamic text. Added relevant CSS, assets, and project structure to start development of a personal portfolio.

---
+++ Naveen_Portfolio/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css'; 
+import Dyxt from './Dynamic_text.jsx';
+const Home = () => {
+    return(
+        <>
+            <div className="main">
+                <Dyxt/>
+            </div>
+        </>
+    )
+}
+
+ export default Home; 
